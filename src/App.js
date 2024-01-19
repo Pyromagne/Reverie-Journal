5ea92a2d83788ba138bb1eee6f205a6905a63bbc
Pyromagne <growganoth826@gmail.com>
2024-01-19 22:49:27 +0800

    Imitate PersistGate of redux (not tested yet)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,33 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import {
+  Home
+} from "./pages";
+import './index.css';
+
+
+const App= ({ persistedState }) => {
+  const [state, setState] = useState(persistedState);
+
+  const handleChange = (event) => {
+    // Update the state when the input changes
+    setState({ data: event.target.value });
+  };
+  
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
+
+/* import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import {
   Home
@@ -18,4 +47,5 @@ function App() {
   );
 }
 
-export default App;
+export default App; */
+
--- src/index.js
+++ src/index.js
@@ -1,4 +1,36 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+const AppWrapper = () => {
+  const [loading, setLoading] = useState(true);
+  const [persistedState, setPersistedState] = useState({});
+
+  useEffect(() => {
+    // Simulate async operation (loading from local storage)
+    const loadPersistedState = async () => {
+      try {
+        const storedState = JSON.parse(localStorage.getItem("appState"));
+        setPersistedState(storedState || {});
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadPersistedState();
+  }, []);
+
+  if (loading) {
+    return null; // You can replace this with a loading spinner or any other loading indicator
+  }
+
+  return <App />;
+};
+
+ReactDOM.render(<AppWrapper />, document.getElementById("root"));
+
+/* import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -8,4 +40,4 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+); */
