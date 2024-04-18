dd49112792a17ec6c09ef2e14cce21d386ab1517
Pyromagne <growganoth826@gmail.com>
2024-04-18 10:42:42 +0800

    Format dreamCard date

--- src/components/DreamCard.jsx
+++ src/components/DreamCard.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { Card, Divider } from "@mui/material";
 import Chips from "./Chip";
+import dayjs from "dayjs";
 
 const truncateDescription = (description, length=200) => {
   return description.length > length ? description.substring(0, length) + '...' : description;
 };
 
+const formatDate = date => {
+  return dayjs(date).format('MM-DD-YYYY');
+}
+
 export const inDreamCard = ({ data: { Title, Date, Description, Tags, Emotions } }) => (
   <Card variant="outlined" className="max-w-full h-auto p-2 m-2 shadow-md">
     <div className="flex justify-between">
       <p className="px-1">{Title}</p>
-      <p className="px-1">{Date}</p>
+      <p className="px-1">{formatDate(Date)}</p>
     </div>
     <Divider className="py-1" />
     <div className="flex flex-col">
@@ -26,8 +31,9 @@ export const inDreamCard = ({ data: { Title, Date, Description, Tags, Emotions }
 export const DreamCard = ({ dream }) => {
   const { Title, Date, Description, Tags, Emotions } = dream;
   const truncDesc = truncateDescription(Description, 275);
+  const formattedDate = formatDate(Date);
 
-  return inDreamCard({ data: { Title, Date, Description: truncDesc, Tags, Emotions } });
+  return inDreamCard({ data: { Title, Date: formattedDate, Description: truncDesc, Tags, Emotions } });
 };
 
 export default DreamCard;
